6d383544f2ab5c84e98d57b06ce39692f8e3e576
bpiwo <bar.piwowarczyk@gmail.com>
2018-11-11 14:20:47 +0100

    Parameters in singAround are now right after __main__ start

--- SingAround.py
+++ SingAround.py
@@ -3,15 +3,20 @@ import numpy as np
 from Waveform import Waveform
 
 
-
-
 if __name__ == '__main__':
     omegaPath = 'omega.npy'
     kvectPath = 'kvect.npy'
     samplingFrequencies = np.linspace(5e3, 1e5, 200)
-    numOfCurves = 3
+    numOfCurves = 4
+    timeOfPropagation = 0.1
+    lengthOfBar = 10.1
+    gain = 1.05
+    numberOfIterations = 100
 
+    # creating waveform
     wave = Waveform(omegaPath, kvectPath, samplingFrequencies, numOfCurves)
-    for i in range(100):
-        wave.propagate(0.1, 2.1, gainFactor=1.05)
+
+    # propagation
+    for i in range(numberOfIterations):
+        wave.propagate(timeOfPropagation, lengthOfBar, gainFactor=gain)
         wave.draw()
